c03f160f266cfd6eb66266e76cb3827f1b9e945c
Darienzhangyy <yz273@duke.edu>
2015-10-23 11:54:49 -0400

    First data clean

--- dataclean1.R
+++ dataclean1.R
@@ -12,15 +12,17 @@ nyc=fread("/home/vis/cr173/Sta523/data/nyc/nyc_311.csv")%>%
 #db = src_sqlite("~cr173/Sta523/data/nyc/nyc_311.sqlite", create = TRUE)
 #nyc_sql = copy_to(db, nyc, temporary = FALSE)
 #str(nyc_sql)
+
+
 #get the address info
 addr = nyc %>%
   select(contains("Address")) %>%
   filter(Incident.Address != "")
 str(addr)
 colnames(addr)[1]="Address"
-
+#Load the pluto data
 load(path.expand("/home/vis/cr173/Sta523/data/nyc/pluto/pluto.Rdata"))
 str(pluto)
 
-
+#keep the data in nyc, left joint pluto data
 Join_Street = left_join(addr, pluto,by="Address")
