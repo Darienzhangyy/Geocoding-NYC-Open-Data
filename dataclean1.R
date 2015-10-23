8aa108642d4f8a12e444813a0cbafb04c9be0ca5
Darienzhangyy <yz273@duke.edu>
2015-10-23 11:37:43 -0400

    first data clean

---
+++ dataclean1.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(data.table)
+library(lubridate)
+library(stringr)
+library(magrittr)
+library(foreign)
+
+nyc=fread("/home/vis/cr173/Sta523/data/nyc/nyc_311.csv")%>%
+  as.data.frame() %>%
+  tbl_df()
+#Transfer to sql file
+#db = src_sqlite("~cr173/Sta523/data/nyc/nyc_311.sqlite", create = TRUE)
+#nyc_sql = copy_to(db, nyc, temporary = FALSE)
+#str(nyc_sql)
+#get the address info
+addr = nyc %>%
+  select(contains("Address")) %>%
+  filter(Incident.Address != "")
+str(addr)
+colnames(addr)[1]="Address"
+
+load(path.expand("/home/vis/cr173/Sta523/data/nyc/pluto/pluto.Rdata"))
+str(pluto)
+
+
+Join_Street = left_join(addr, pluto,by="Address")
