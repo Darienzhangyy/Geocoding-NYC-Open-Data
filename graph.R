81a5023c77784ef93d1b1617c2dbe9701ed7b501
Darienzhangyy <yz273@duke.edu>
2015-10-24 17:30:52 -0400

    Add more data cleaning _ average long and lat

--- graph.R
+++ graph.R
@@ -5,7 +5,7 @@ library(stringr)
 library(magrittr)
 library(foreign)
 
-nyc=fread("/home/vis/cr173/Sta523/data/nyc/nyc_311.csv")%>%
+nyc=fread("/home/vis/cr173/Sta523/data/nyc/nyc_311.csv")%>% 
   as.data.frame() %>%
   tbl_df()
 #Transfer to sql file
@@ -25,17 +25,21 @@ str(pluto)
 
 Join_Street=left_join(addr, pluto,by="Address")
 #delete rows with NA longtitude or latitude
+#Addresses in the same boroughs, average long and lat
 Join_Street_clean = Join_Street %>%
-  filter(!is.na(x))
+  select(Address,x,y,Borough)%>%
+  filter(!is.na(x))%>%
+  group_by(Address,Borough)%>%
+  summarise(x=mean(x),y=mean(y))
 
 library(graphics)
 
-plot(c(Join_Street_clean[,4],Join_Street_clean[,5]), cex = 0.5, col=as.factor(Join_Street_clean$Borough))
+plot(c(Join_Street_clean[,3],Join_Street_clean[,4]), cex = 0.5, col=as.factor(Join_Street_clean$Borough))
 
-hpts1=chull(Join_Street_clean[which(Join_Street_clean[,7]=="BK"),4:5])
+hpts1=chull(Join_Street_clean[which(Join_Street_clean$Borough=="BK"),3:4])
 hpts1 <- c(hpts1, hpts1[1])
-lines(Join_Street_clean[hpts1,4:5])
+lines(Join_Street_clean[hpts1,3:4])
 
-hpts2=chull(Join_Street_clean[which(Join_Street_clean[,7]=="QN"),4:5])
+hpts2=chull(Join_Street_clean[which(Join_Street_clean$Borough=="QN"),3:4])
 hpts2 <- c(hpts2, hpts2[1])
 lines(Join_Street_clean[hpts2,4:5])
