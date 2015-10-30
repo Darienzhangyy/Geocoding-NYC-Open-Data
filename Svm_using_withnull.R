fad2e5d4bb0a62cb83073d881214dc5c609c4f6b
Darienzhangyy <yz273@duke.edu>
2015-10-30 17:34:11 -0400

    file for new json

---
+++ Svm_using_withnull.R
@@ -0,0 +1,54 @@
+load("with_null.RData")
+
+############ SVM ############
+
+library(e1071)
+library(raster)
+
+r = raster(nrows=500, ncols=500,
+           xmn=-74.3, xmx=-73.71,
+           ymn=40.49, ymx=40.92)
+r[]=NA
+
+
+short_to_long = c("BK"="Brooklyn",
+                  "BX"="Bronx",
+                  "MN"="Manhattan",
+                  "QN"="Queens",
+                  "SI"="Staten Island")
+
+
+Samp <- full_data[sample(1:nrow(full_data), 15000, replace=FALSE),]
+#SVM
+SvmNyc1 <- svm(as.factor(knn) ~ Longitude + Latitude, data = Samp,
+               cost= 20000, gamma=2)
+
+
+
+
+# make predictions
+pred_locs = data.frame(xyFromCell(r, 1:250000))
+names(pred_locs) = c("Longitude","Latitude")#match name in the svm model
+
+pred = predict(SvmNyc1,pred_locs)
+r[] = pred
+plot(r)
+## Create Polygons
+
+library(rgeos)
+poly = rasterToPolygons(r,dissolve=TRUE)
+
+
+
+names(poly@data) = "Name"
+poly@data$Name = short_to_long[levels(pred)]
+
+setwd("/home/grad/yz273/bootcamp/")
+source("write_geojson.R")
+write_geojson(poly,"boroughs.json")
+
+
+
+
+
+##################
