333ebe5295f9cbf800706b40700dc029b932459c
Darienzhangyy <yz273@duke.edu>
2015-10-30 01:12:51 -0400

    Tried SVM

--- graph.R
+++ graph.R
@@ -36,9 +36,86 @@ library(graphics)
 
 plot(c(Join_Street_clean[,3],Join_Street_clean[,4]), cex = 0.5, col=as.factor(Join_Street_clean$Borough))
 
-hpts1=chull(Join_Street_clean[which(Join_Street_clean$Borough=="BK"),3:4])
+
+
+############ SVM ############
+
+library(e1071)
+library(raster)
+
+r = raster(nrows=500, ncols=500,
+           xmn=-74.3, xmx=-73.71,
+           ymn=40.49, ymx=40.92)
+r[]=NA
+
+
+short_to_long = c("BK"="Brooklyn",
+                  "BX"="Bronx",
+                  "MN"="Manhattan",
+                  "QN"="Queens",
+                  "SI"="Staten Island")
+
+
+
+# sample from the data
+Samp <- Join_Street_clean[sample(1:nrow(Join_Street_clean), 2000, replace=FALSE),]
+test <- Join_Street_clean[sample(1:nrow(Join_Street_clean), 5000, replace=FALSE),]
+#SVM
+SvmNyc <- svm(as.numeric(as.factor(Borough)) ~ x + y, data = Samp)
+
+
+
+
+# make predictions
+pred_locs = data.frame(xyFromCell(r, 1:250000))
+names(pred_locs) = c("long","lat")
+
+pred = predict(SvmNyc,test)
+r[] = pred
+
+## Create Polygons
+
+library(rgeos)
+poly = rasterToPolygons(r,dissolve=TRUE)
+
+
+plot(poly)
+
+
+
+
+
+##################
+
+plot(c(Join_Street_clean[,3],Join_Street_clean[,4]), cex = 0.5, col=as.factor(Join_Street_clean$Borough))
+
+
+mysample <- Join_Street_clean[sample(1:nrow(Join_Street_clean), 2000, replace=FALSE),]
+num_Bor = as.numeric(as.factor(mysample$Borough))
+SVM = svm(num_Bor  ~ x + y, data= mysample, cost= 8, gamma=2)
+
+
+
+library(raster)
+r = raster(nrows=500, ncols=500, 
+           xmn=-74.3, xmx=-73.71, 
+           ymn=40.49, ymx=40.92)
+r[]=NA
+
+
+pred_locs = data.frame(xyFromCell(r, 1:250000))
+
+preds = predict(SVM, pred_locs)
+r[] = preds
+poly = rasterToPolygons(r,dissolve=TRUE)
+
+
+
+
+
+hpts1=chull(Samp[which(Samp$Borough=="BK"),3:4])
 hpts1 <- c(hpts1, hpts1[1])
-lines(Join_Street_clean[hpts1,3:4])
+lines(Samp[hpts1,3:4])
 
 hpts2=chull(Join_Street_clean[which(Join_Street_clean$Borough=="QN"),3:4])
 hpts2 <- c(hpts2, hpts2[1])
